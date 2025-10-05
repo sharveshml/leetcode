a822c7776e297ba76d30d55ee19a4d5ff606b98e
sharvesh <sharvesh1601@gmail.com>
2025-10-05 13:15:46 +0530

    Added binary search approach

--- Medium/74. Search a 2D Matrix.py
+++ Medium/74. Search a 2D Matrix.py
@@ -10,3 +10,25 @@ class Solution:
             else:
                 m = m-1
         return False
+
+# Binary Search Approach
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n, m = len(matrix), len(matrix[0])
+        left, right = 0, n*m-1
+
+        while left <= right:
+            mid = left+(right-left) // 2
+            num = matrix[mid // m][mid % m]
+
+            if num == target:
+                return True
+            
+            elif target < num:
+                right = mid - 1
+
+            else:
+                left = mid+1
+        return False
+
+        
