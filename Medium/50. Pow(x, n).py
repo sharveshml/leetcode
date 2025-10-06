303c49bbb4c9ecf1532870b5691bf7c723f427c1
Sharvesh <sharvesh@Sharveshs-MacBook-Air.local>
2025-10-06 23:21:29 +0530

    Added pow solution

---
+++ Medium/50. Pow(x, n).py
@@ -0,0 +1,38 @@
+# TLE
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        ans = 1
+
+        if n == 0:
+            return 1
+
+        if n < 0:
+            x = 1/x
+            n = -n
+
+        for i in range(n):
+            ans *= x
+
+        return ans
+
+# Optimized ( Law of Exponents )
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        result = 1
+
+        if n == 0:
+            return 1
+
+        if n < 0:
+            x = 1/x
+            n = -n
+
+        while n > 0:
+
+            if n % 2 != 0:
+                result *= x
+
+            x *= x
+            n //=  2
+
+        return result
